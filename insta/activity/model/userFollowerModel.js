8790488d95a71d13fd87cf592d11ae7f57752258
Shubam <shubhamparashar210@gmail.com>
2020-09-08 16:46:00 +0530

    accept & reject req

--- insta/activity/model/userFollowerModel.js
+++ insta/activity/model/userFollowerModel.js
@@ -11,4 +11,30 @@ const createRequest = function (mappingObj) {
         });
     })
 }
+const accpectRequest = function (user_id, follower_id) {
+    return new Promise(function (resolve, reject) {
+        db.query(`UPDATE user_follower SET is_pending=0 WHERE user_id="${user_id}" AND follower_id="${follower_id}"`, function (err, result) {
+            // Neat!
+            if (err) {
+                reject(err)
+            } else {
+                resolve(mappingObj);
+            }
+        });
+    })
+}
+const rejectRequest = function (user_id, follower_id) {
+    return new Promise(function (resolve, reject) {
+        db.query(`DELETE from user_follower WHERE user_id="${user_id}" AND follower_id="${follower_id}" AND is_pending=1`, function (err, result) {
+            // Neat!
+            if (err) {
+                reject(err)
+            } else {
+                resolve(mappingObj);
+            }
+        });
+    })
+}
 module.exports.createRequest = createRequest;
+module.exports.accpectRequest  = accpectRequest;
+module.exports.rejectRequest =  rejectRequest;
--- insta/activity/router/userRouter.js
+++ insta/activity/router/userRouter.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const userRouter = new express.Router();
-let { createUser, updateUser, deleteUser, getUser, getAllUser, handleRequest } = require("../controller/userController");
+let { createUser, updateUser, deleteUser, getUser,
+     getAllUser, handleRequest, acceptRequest, rejectRequest } = require("../controller/userController");
 // user routes
 // /:user_id
 // read  => GET ONE 
@@ -8,4 +9,5 @@ let { createUser, updateUser, deleteUser, getUser, getAllUser, handleRequest } =
 userRouter.route("/").post(createUser).get(getAllUser)
 userRouter.route("/fr").post(handleRequest)
 userRouter.route("/:user_id").get(getUser).patch(updateUser).delete(deleteUser);
+userRouter.route("/fr/:user_id/:follower_id").patch(acceptRequest).delete(rejectRequest;)
 module.exports = userRouter
